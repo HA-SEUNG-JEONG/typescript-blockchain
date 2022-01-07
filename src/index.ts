38ffd9e6fe0041842d20c4a44fe490dd43ca68e6
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-07 14:21:44 +0900

    Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 /**
- * @constructor name,age,gender
+ * @variable index,hash,previousHash,data,timestamp
  */
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female");
+/** genesisBlock is new Block */
+const genesisBlock: Block = new Block(0, "2020202020", "", "hello", 123433542);
 
-/**
- * print a name,age,gender
- *
- * @param version name,age,gender
- */
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+/**`blockChain`: the array of `Block` Class */
+let blockChain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockChain);
 
 export {}; // 이 파일이 모듈이 될 수 있도록 고침
