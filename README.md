6db15b7b5cf12529124b1a1efc011adf70c693dc
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-07 14:09:57 +0900

    class, public variable,private variable

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ parameter 뒤에 ?를 추가하면 굳이 함수의 parameter로 넘길 필요�
 #### interface
 
 object를 넘기고자 할때 Typescript가 object를 이해하고 올바른 타입인지 확인할 수 있도록 해야한다.
+
+#### class
+
+interface들은 js 파일로 컴파일 되지 않는다.
+
+이를 대신해서 class를 사용한다.
+
+js에서는 클래스가 어떤 속성을 가지는지 선언할 필요가 없지만 ts에서는 꼭 필요한 작업이다.
+
+class에서 private 변수를 사용하면 class 바깥에서는 해당 변수 사용 불가능
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 /**
- * types definition of `person`
+ * @constructor name,age,gender
  */
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "jeong",
-  age: 28,
-  gender: "male",
-};
+const lynn = new Human("Lynn", 18, "female");
 
 /**
  * print a name,age,gender
@@ -22,6 +23,6 @@ const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {}; // 이 파일이 모듈이 될 수 있도록 고침
